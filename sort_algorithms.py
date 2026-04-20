44bc218e5496c012e38377a3d4aa96be4f7b5868
WN <7205768wangnan@163.com>
2026-04-20 17:59:35 +0800

    chore: remove buble_sort function

--- sort_algorithms.py
+++ sort_algorithms.py
@@ -8,20 +8,3 @@ def quick_sort(arr):
     middle = [x for x in arr if x == pivot]
     right = [x for x in arr if x > pivot]
     return quick_sort(left) + middle + quick_sort(right)
-
-
-def bubble_sort(arr):
-    """Return a new list sorted with bubble sort."""
-    result = arr[:]
-    n = len(result)
-
-    for i in range(n):
-        swapped = False
-        for j in range(0, n - i - 1):
-            if result[j] > result[j + 1]:
-                result[j], result[j + 1] = result[j + 1], result[j]
-                swapped = True
-        if not swapped:
-            break
-
-    return result
