16668611c53d5225f6284522a8d3a33b768e3f1a
WN <7205768wangnan@163.com>
2026-04-22 17:12:02 +0800

    test: trim to one function on test-north-coder-rfc-0060-e2e-090947

--- sample_functions.py
+++ sample_functions.py
@@ -12,19 +12,3 @@ def normalize_name(name):
             normalized_parts.append(part[0].upper() + part[1:].lower())
 
     return " ".join(normalized_parts)
-
-
-def collect_even_numbers(values):
-    even_numbers = []
-    for value in values:
-        if isinstance(value, bool):
-            continue
-        if not isinstance(value, int):
-            continue
-        if value % 2 != 0:
-            continue
-
-        even_numbers.append(value)
-
-    even_numbers.sort()
-    return even_numbers
